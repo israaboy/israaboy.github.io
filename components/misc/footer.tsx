012e4f3724b5b4336d1e296552149b946373fc09
israaboy <israel.t.ribeiro@gmail.com>
2024-09-24 10:42:29 -0300

    update footer organization

--- components/misc/footer.tsx
+++ components/misc/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <div className="container mx-auto px-5">
-        <div className="py-28 flex flex-col lg:flex-row items-end">
+        <div className="py-28 flex flex-col lg:flex-row items-center w-full">
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="#"
